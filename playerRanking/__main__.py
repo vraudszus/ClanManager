6a516c91fee1f8daf68f8d7c66e29e7f9cf9dfd7
Vincent Raudszus <vincent.raudszus@gmail.com>
2022-10-01 19:57:15 +0200

    Make plot optional with cli argument

--- playerRanking/__main__.py
+++ playerRanking/__main__.py
@@ -1,4 +1,11 @@
+import argparse
 from playerRanking import playerRanking
 
+ARGUMENT_PARSER = argparse.ArgumentParser()
+ARGUMENT_PARSER.add_argument("-p", "--plot",
+                             help="Plot the rating history to a file",
+                             action="store_true")
+
 if __name__ == "__main__":
-    playerRanking.perform_evaluation()
+    args = ARGUMENT_PARSER.parse_args()
+    playerRanking.perform_evaluation(plot=args.plot)
